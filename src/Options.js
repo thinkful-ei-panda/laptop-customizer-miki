e95ce2894621b344e5a7f7b8a8ce663acdeef9ea
mischkiey <mischkieycf@gmail.com>
2020-06-20 08:40:27 -0400

    May do another refactor but code works as expected

---
+++ src/Options.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import slugify from 'slugify';
+
+function Options(props) {
+
+    const USCurrencyFormat = new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD'
+      });
+
+    return props.options.map(item => {
+        const itemHash = slugify(JSON.stringify(item));
+
+        return (
+          <div key={itemHash} className="feature__item">
+            <input
+              type="radio"
+              id={itemHash}
+              className="feature__option"
+              name={slugify(props.feature)}
+              // checked if blah
+              checked={item.name === props.selected[props.feature].name}
+              onChange={e => props.updateFeature(props.feature, item)}
+            />
+            <label htmlFor={itemHash} className="feature__label">
+              {item.name} ({USCurrencyFormat.format(item.cost)})
+            </label>
+          </div>
+        );
+    });
+}
+
+export default Options;
---
+++ src/Total.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function Total(props) {
+
+    const USCurrencyFormat = new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD'
+    });
+
+    const total = Object.keys(props.selected).reduce(
+        (acc, curr) => acc + props.selected[curr].cost,
+        0
+    );
+    
+    return (
+        <div className="summary__total__value">
+            {USCurrencyFormat.format(total)}
+        </div>
+    )
+}
+
+export default Total;
